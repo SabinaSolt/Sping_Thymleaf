3ede8b26aaad11274124dced6aab2c15f34181d7
SabinaSolt <sabina.soltobaeva@gmail.com>
2020-12-10 14:09:30 +0100

    Consommer API REST

--- src/main/java/donjons/feuillethym/controller/MainController.java
+++ src/main/java/donjons/feuillethym/controller/MainController.java
@@ -6,22 +6,23 @@ import java.util.List;
 
 import donjons.feuillethym.form.PersonForm;
 import donjons.feuillethym.model.Person;
+import donjons.feuillethym.model.PersonList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MainController {
 
-    private static List<Person> persons = new ArrayList<Person>();
-
-    static {
-        persons.add(new Person(1,"Bill", "Gates"));
-        persons.add(new Person(2,"Steve", "Jobs"));
-    }
+    @Autowired
+    private RestTemplate restTemplate;
 
     // Injectez (inject) via application.properties.
     @Value("${welcome.message}")
@@ -38,20 +39,27 @@ public class MainController {
         return "index";
     }
 
+    public List<Person> getPersonsListApi() {
+        PersonList personList= restTemplate.getForObject("http://localhost:8081/Personnages",PersonList.class);
+        return personList.getPersonList();
+    }
+
     @RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
     public String personList(Model model) {
-
-        model.addAttribute("persons", persons);
-
+        model.addAttribute("persons",getPersonsListApi());
         return "personList";
+    }
+    @RequestMapping(value = { "/personList/{id}" }, method = RequestMethod.GET)
+    public String displayPerson(Model model, @PathVariable int id) {
+        Person person=restTemplate.getForObject("http://localhost:8081/Personnages/"+id,Person.class);
+        model.addAttribute("person", person);
+        return "displayPerson";
     }
 
     @RequestMapping(value = { "/addPerson" }, method = RequestMethod.GET)
     public String showAddPersonPage(Model model) {
-
         PersonForm personForm = new PersonForm();
         model.addAttribute("personForm", personForm);
-
         return "addPerson";
     }
 
@@ -65,8 +73,8 @@ public class MainController {
         if (name != null && name.length() > 0 //
                 && type != null && type.length() > 0 && id>-1) {
             Person newPerson = new Person(id,name, type);
-            persons.add(newPerson);
-
+            restTemplate.postForEntity("http://localhost:8081/Personnages",newPerson, ResponseEntity.class);
+            model.addAttribute("persons",getPersonsListApi());
             return "redirect:/personList";
         }
 
---
+++ src/main/java/donjons/feuillethym/model/PersonList.java
@@ -0,0 +1,18 @@
+package donjons.feuillethym.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonList {
+    private List<Person> personList;
+
+
+
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public void setPersonList(List<Person> personList) {
+        this.personList = personList;
+    }
+}
